4126287acb5dc32beeaf90dea4b15badd55dbdcc
Rohit Sriram <rohitsram10@outlook.com>
2026-01-05 16:41:49 -0500

    data loading and embedding done.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name="Medical Agent",
+  version="0.1.0",
+  author="rohitsram",
+  author_email="rohitcsdev79@gmail.com ",
+  packages=find_packages(),
+  install_requires=[]
+)
